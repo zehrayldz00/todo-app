7cf924d5fb8b3dfc1427231915c753b2822f4c3b
Zehra Gul Yildiz <zehragul15@gmail.com>
2025-02-25 23:18:02 +0300

    todo application with Flutter

--- lib/core/api_service.dart
+++ lib/core/api_service.dart
@@ -1,17 +1,11 @@
-/*import 'dart:convert';
-import 'dart:io';
 
-import 'package:http/http.dart' as http;
-import 'package:logger/logger.dart';
-
-import 'model/task.dart';*/
 
 class ApiService {
-  /*late String _baseUrl;
+  //late String _baseUrl;
 
   final ApiService _instance = ApiService._privateConstructor();
   ApiService._privateConstructor() {
-    _baseUrl = "https://rest-8692d-default-rtdb.firebaseio.com/";
+    //_baseUrl = "https://rest-8692d-default-rtdb.firebaseio.com/";
   }
   //eager ile sarmalamak ne demek
 
@@ -19,7 +13,7 @@ class ApiService {
     return _instance;
   }
 
-  Future<List<Task>> getTasks() async {
+  /*Future<List<Task>> getTasks() async {
     final response = await http.get(Uri.parse("$_baseUrl/task.json"));
 
     final jsonResponse = json.decode(response.body);
