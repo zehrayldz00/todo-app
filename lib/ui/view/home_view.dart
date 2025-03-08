6a07cae288a66199ce40631c99f3a700b43d1e08
Zehra Gul Yildiz <zehragul15@gmail.com>
2025-03-08 23:37:03 +0300

    todo application with Flutter dismissible card adjusted.

--- lib/ui/view/home_view.dart
+++ lib/ui/view/home_view.dart
@@ -89,35 +89,27 @@ class _HomeViewState extends State<HomeView> {
       itemBuilder: (context, index) => _dismissibleCard(taskList[index]));
 
   Widget _dismissibleCard(Task task) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: Dismissible(
-        key: UniqueKey(),
-        background: Container(
-          alignment: Alignment.centerRight,
-          margin: EdgeInsets.only(bottom: 20, top: 10),
-          padding: EdgeInsets.only(right: 20),
-          color: Colors.red,
-          child: Icon(
-            Icons.delete,
-            color: white,
-            size: 30,
-          ),
+    return Dismissible(
+      key: UniqueKey(),
+      background: Container(
+        alignment: Alignment.centerRight,
+        color: Colors.red,
+        child: Icon(
+          Icons.delete,
+          color: white,
+          size: 30,
         ),
-        child: Container(
-          height: 100,
-          child: CustomCard(
-              title: task.taskDetail,
-              subtitle: task.taskDetail,
-              cardColor: cardColor),
-        ),
-        onDismissed: (direction) async {
-          await service.removeTasks(task.key!);
-          setState(() {
-            taskList.remove(task);
-          });
-        },
       ),
+      child: CustomCard(
+          title: task.taskDetail,
+          subtitle: task.taskDetail,
+          cardColor: cardColor),
+      onDismissed: (direction) async {
+        await service.removeTasks(task.key!);
+        setState(() {
+          taskList.remove(task);
+        });
+      },
     );
   }
 
