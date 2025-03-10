9bd2a4706c21eb400b541c536a29b4df1525d8a7
Zehra Gul Yildiz <zehragul15@gmail.com>
2025-03-10 23:24:34 +0300

    todo application with Flutter dismissible card adjusted.

--- lib/ui/view/home_view.dart
+++ lib/ui/view/home_view.dart
@@ -89,31 +89,27 @@ class _HomeViewState extends State<HomeView> {
       itemBuilder: (context, index) => _dismissibleCard(taskList[index]));
 
   Widget _dismissibleCard(Task task) {
-    return SizedBox(
-      height: 100,
-      child: Dismissible(
-        direction: DismissDirection.endToStart,
-        key: UniqueKey(),
-        background: Container(
-          alignment: Alignment.centerRight,
-          color: Colors.red,
-          child: Icon(
-            Icons.delete,
-            color: white,
-            size: 30,
-          ),
+    return Dismissible(
+      key: UniqueKey(),
+      background: Container(
+        alignment: Alignment.centerRight,
+        color: Colors.red,
+        child: Icon(
+          Icons.delete,
+          color: white,
+          size: 30,
         ),
-        child: CustomCard(
-            title: task.taskDetail,
-            subtitle: task.taskDetail,
-            cardColor: cardColor),
-        onDismissed: (direction) async {
-          await service.removeTasks(task.key!);
-          setState(() {
-            taskList.remove(task);
-          });
-        },
       ),
+      child: CustomCard(
+          title: task.taskDetail,
+          subtitle: task.taskDetail,
+          cardColor: cardColor),
+      onDismissed: (direction) async {
+        await service.removeTasks(task.key!);
+        setState(() {
+          taskList.remove(task);
+        });
+      },
     );
   }
 
